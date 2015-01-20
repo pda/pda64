55424d73916920e490e2e464037b59cd1209bf73
Paul Annesley <paul@annesley.cc>
2015-01-20 00:14:30 -0800

    boot.s: INT10h AH=0Eh to output message one char at a time.

--- boot.s
+++ boot.s
@@ -1,16 +1,19 @@
+bits 16
 section .text
 org 0x7C00
 
-write_string:
-	mov	ah, 0x13	; int10h 13h: display string
-	mov	bl, 0x0E	; color, E=yellow on 0=black
-	mov	al, 0x00	; write-mode
-	mov	cx, 11		; string length
-	mov	dh, 24		; screen row
-	mov	dl, 0		; screen column
-	push	0
-	pop	es		; string address segment
-	mov	bp, message	; string address
-	int	0x10
+write_message_tty_style:
+	mov	bx, 0		; index into message
+write_message_tty_style_loop:
+	mov	al, [message+bx]	; BIOS: write_char param
+	mov	ah, 0x0E		; BIOS: output character, TTY style.
+	mov	bh, 0x00		; BIOS: page number
+	int	0x10			; BIOS: call
+	inc	bx
+	cmp	bx, after_message-message
+	jb	write_message_tty_style_loop
+
+
 message:
-	db "hello pda64"
+	db "pda64 booting..."
+after_message:
